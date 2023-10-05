2ac278175ceb529e63e5ab69533e897aebd44dbc
IanPomaz <128009980+IanPomaz@users.noreply.github.com>
2023-10-05 13:15:12 -0400

    footer and about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,8 @@
 import './About.css'
 
 const About = () =>
-    <div className='about'>
-        <h1>About Me</h1>
-    </div>
-
+<div>
+<h2>About Me</h2>
+<p>Hey there! I'm Ian, a former small-business owner who started my career at 19, remodeling homes. Lately, I've been diving headfirst into the world of web development and coding. With a background rooted in construction precision, problem-solving, and teamwork, I've seamlessly transitioned these skills into the digital sphere. I'm proficient particularly in JavaScript, spanning from front-end to back-end development.</p>
+</div>
 export default About;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,19 @@
-import './Footer.css'
+import styles from './Footer.css';
 
-const Footer = ({setPage}) =>
-    <footer className='Footer'>
-        <h1>Footer</h1>
-    </footer>
+const Footer = () => (
+  <footer className={styles.footer}>
+    <div className={styles.footerContent}>
+      <p className={styles.footerText}>focusfolio</p>
+      <div className={styles.footerLinks}>
+        <a href="/about" className={styles.footerLink}>
+          About Us
+        </a>
+        <a href="/contact" className={styles.footerLink}>
+          Contact Me
+        </a>
+      </div>
+    </div>
+  </footer>
+);
 
 export default Footer;
